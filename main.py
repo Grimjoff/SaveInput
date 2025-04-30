0a47833f3f2014d95fcbfec725bdba2f6b3dce03
BuildTools <unconfigured@null.spigotmc.org>
2025-05-01 00:25:01 +0200

    Cleanup, commented out mouse_listener for now

--- main.py
+++ main.py
@@ -14,20 +14,6 @@ timeout = 3  # Seconds before inserting "/"
 stop_event = threading.Event()
 DB_PATH = "Database.db"
 
-# button_replacements = {
-#     "Button.left": "LC",  # Change left click to "LC"
-#     "Button.right": "RC",  # Change right click to "RC"
-#     "Key.space": " ",
-#     "Key.enter": "Enter",
-#     "Key.tab":"Tab",
-#     "Key.backspace": "<-",
-#     "Key.ctrl_l": "CTRL",
-#     "Key.alt_l": "alt",
-#     "Key.shift": "SH",
-#     "Button.x1": "M1",
-#     "Button.middle": "mid",
-#     "Key.esc": "esc"
-# }
 # Function to log key presses with the time and key/button name
 def get_active_window():
     try:
@@ -38,12 +24,7 @@ def get_active_window():
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         return "Unknown"
 
-buffer = []
-csv_file = "input_log.csv"
 held_keys = set()
-# def transform_button_name(button):
-#     return button_replacements.get(button, button)
-# Function to log key presses
 def on_key_press(key):
     if get_active_window() == "Discord.exe":
         global held_keys
@@ -75,28 +56,25 @@ def on_key_release(key):
 
 
 # Function to log mouse clicks with the time and button
-def on_click(x, y, button, pressed):
-    if pressed:
-        program = get_active_window()
-        button_name = str(button)  # Get the button name
-        #button_name = transform_button_name(button_name)
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current time
-        global df
-        # Append new entry to the dataframe
-        new_entry = pd.DataFrame([[timestamp, button_name, program]], columns=["Time", "Button", "Process"])
-        # Append the new entry to the CSV without header
-        new_entry.to_csv(csv_file, mode='a', header=not pd.io.common.file_exists(csv_file), index=False)
+# def on_click(x, y, button, pressed):
+#     if pressed:
+#         program = get_active_window()
+#         button_name = str(button)  # Get the button name
+#         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current time
+#         global df
+#         # Append new entry to the dataframe
+#         # Append the new entry to the CSV without header
 
 
 # Start keyboard and mouse listeners
 keyboard_listener = keyboard.Listener(on_press=on_key_press, on_release=on_key_release)
-mouse_listener = mouse.Listener(on_click=on_click, on_release=on_key_release)
+#mouse_listener = mouse.Listener(on_click=on_click, on_release=on_key_release)
 
 keyboard_listener.start()
-mouse_listener.start()
+#mouse_listener.start()
 
 keyboard_listener.join()
-mouse_listener.join()
+#mouse_listener.join()
 
 conn = sqlite3.connect('Database.db')
 cursor = conn.cursor()
